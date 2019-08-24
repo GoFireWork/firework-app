5d6bed2a2a1e12ae409c3396c7eca0f97373da75
JohnAllen <johnjalleniii@gmail.com>
2019-08-24 10:02:25 -0700

    Move runTests into Tests/index

--- app/containers/Tests/index.js
+++ app/containers/Tests/index.js
@@ -9,13 +9,26 @@ import { makeSelectNumTests, makeSelectAllTests } from './selectors';
 
 import TestsList from './components/TestsList/TestsList';
 import { SelectedIssueContext } from '../WorkPage/SelectedIssueContextWrapper';
-import { runTests } from './runTests';
+// import { runTests } from './runTests';
+import { updateTestsResults } from './actions';
+import { run } from 'jest-lite';
+
+function requireAll(r) {
+  r.keys().forEach(r);
+}
 
 export function Tests(props) {
   const value = useContext(SelectedIssueContext);
   const { selectedIssueID } = value;
   useEffect(() => {
     if (selectedIssueID !== 0) {
+      // eslint-disable-next-line no-inner-declarations
+      function runTests() {
+        requireAll(require.context('./tests/expTests/', true, /\.js$/));
+        const results = run();
+        console.log(`${results}`);
+        updateTestsResults(results);
+      }
       runTests();
     }
   }, Object.values(value));
--- app/containers/Tests/runTests.js
+++ app/containers/Tests/runTests.js
@@ -5,9 +5,9 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
-export const runTests = async () => {
-  requireAll(require.context('./tests/expTests/', true, /\.js$/));
-  const results = await run();
-  // console.log(`${results}`);
-  updateTestResults(results);
-};
+// export const runTests = async () => {
+//   requireAll(require.context('./tests/expTests/', true, /\.js$/));
+//   const results = await run();
+//   console.log(`${results}`);
+//   return results;
+// };
