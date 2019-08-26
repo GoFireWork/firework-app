62feb5c01893820283e8a7d341bdb9436decfd0d
JohnAllen <johnjalleniii@gmail.com>
2019-08-26 07:51:46 -0700

    Tests rerunning on issue select

--- app/containers/Tests/actions.js
+++ app/containers/Tests/actions.js
@@ -1,6 +1,9 @@
 import { run } from 'jest-lite';
 import { TESTS_RESULTS } from './constants';
 
+import configureStore from '../../configureStore';
+const store = configureStore();
+
 export const setTestsResults = testsResults => ({
   type: TESTS_RESULTS,
   testsResults,
@@ -10,9 +13,10 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
-export const runTests = () => async dispatch => {
+export const runTests = async () => {
+  console.log(`Running tests`);
   requireAll(require.context('./tests/expTests/', true, /\.js$/));
   const results = await run();
-  console.log(`${results.length}`);
-  dispatch(setTestsResults(results));
+  console.log(`${results.length} tests found`);
+  store.dispatch(setTestsResults(results));
 };
