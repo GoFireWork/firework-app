830499c673a0b0b7baa36f99d7a66162d04bfa78
JohnAllen <johnjalleniii@gmail.com>
2019-09-03 16:44:18 -0700

    Fix testing conflicts

--- app/containers/Tests/actions.js
+++ app/containers/Tests/actions.js
@@ -1,6 +1,19 @@
+import { run } from 'jest-lite';
 import { TESTS_RESULTS } from './constants';
 
 export const setTestsResults = testsResults => ({
   type: TESTS_RESULTS,
   testsResults,
 });
+
+function requireAll(r) {
+  r.keys().forEach(r);
+}
+
+export const runTests = async () => {
+  console.log(`Running tests`);
+  requireAll(require.context('./tests/expTests/', true, /\.js$/));
+  const results = await run();
+  console.log(`${results.length} total tests found`);
+  return results;
+};
