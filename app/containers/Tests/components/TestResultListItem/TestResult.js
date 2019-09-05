406b5e060c93540abbcc5cc62824f13a18231077
JohnAllen <johnjalleniii@gmail.com>
2019-09-05 16:47:46 -0700

    Improve styling of TestRestListItem and add jest-lite file

--- app/containers/Tests/components/TestResultListItem/TestResult.js
+++ app/containers/Tests/components/TestResultListItem/TestResult.js
@@ -6,7 +6,11 @@ export const TestResult = styled.div`
   width: 100%;
   height: 100%;
   align-items: center;
-  padding: 0 1em;
+  padding: 0.5em;
   background: ${props =>
     props.testResult.status === 'fail' ? 'red' : 'green'};
+
+  & :not(:last-child) {
+    border-bottom: 1px solid ${'rgba(0, 0, 0, 0.8)'};
+  }
 `;
