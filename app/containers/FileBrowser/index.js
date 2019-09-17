89c28068552beb699c5cdc7d6618c41658242f18
shubh4solanki <shubh4solanki@gmail.com>
2019-09-17 17:00:49 +0530

    Allow user to create file and folder

--- app/containers/FileBrowser/index.js
+++ app/containers/FileBrowser/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect, memo } from 'react';
+import React, { useEffect, memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
+import { ic_create_new_folder as createFolder } from 'react-icons-kit/md/ic_create_new_folder';
+import { ic_insert_drive_file as createFile } from 'react-icons-kit/md/ic_insert_drive_file';
 
 import { useInjectReducer } from 'utils/injectReducer';
 import { useInjectSaga } from 'utils/injectSaga';
+import Icon from 'react-icons-kit';
 import { setFetchingFiles, selectFile } from './actions';
 import {
   makeSelectFiles,
@@ -15,7 +18,7 @@ import {
 import reducer from './reducer';
 import saga from './saga';
 import TreeView from './components/TreeView';
-import { FileBrowser } from './styles';
+import { FileBrowser, WrappenHeader } from './styles';
 
 const key = 'files';
 
@@ -23,12 +26,21 @@ export function FileBrowserContainer(props) {
   useInjectReducer({ key, reducer });
   useInjectSaga({ key, saga });
 
+  const [click, setClick] = useState('');
+
   useEffect(() => {
     props.setFetchingFiles(props.repo);
   }, []);
-
+  console.log(click);
   return (
     <FileBrowser>
+      <WrappenHeader>
+        <span>Explore</span>
+        <div>
+          <Icon icon={createFile} onClick={() => setClick('file')} />
+          <Icon icon={createFolder} onClick={() => setClick('folder')} />
+        </div>
+      </WrappenHeader>
       <TreeView {...props} />
     </FileBrowser>
   );
