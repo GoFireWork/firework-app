365f495580a56c48869a6be826d49302810e5f7a
Keena7 <manjrekarkena7@gmail.com>
2019-09-23 15:48:38 +0530

    Remove console log

--- app/keyboardJS.js
+++ app/keyboardJS.js
@@ -1,14 +1,12 @@
 import keyboardJS from 'keyboardjs';
 import { saveCurrentFile, setSearchFile } from './containers/Editor/actions';
 
-keyboardJS.bind('ctrl + s', async e => {
+keyboardJS.bind(['ctrl + s', 'command + s'], async e => {
   e.preventDefault();
-  console.log('command + s pressed; saving file');
-  await saveCurrentFile();
+  window.store.dispatch(saveCurrentFile());
 });
 
-keyboardJS.bind('ctrl + f', async e => {
+keyboardJS.bind(['ctrl + f', 'command + f'], async e => {
   e.preventDefault();
-  console.log('command + f pressed; saving file');
   window.store.dispatch(setSearchFile(true));
 });
