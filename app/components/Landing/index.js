1e380255ba2d08b3c4fb9309cc51e1c7d59424a0
JohnAllen <johnjalleniii@gmail.com>
2019-09-25 08:33:37 +0200

    Massive landing improvements

--- app/components/Landing/index.js
+++ app/components/Landing/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import { Provider, Heading, Subhead } from 'rebass';
 import {
+  Box,
+  Feature,
+  Flex,
   Hero,
-  CallToAction /* , ScrollDownIndicator */,
+  CallToAction,
+  Section,
 } from 'react-landing-page';
 import Mailchimp from 'react-mailchimp-form';
-import MailChimpForm from './MailChimpForm';
+
+import { MailChimpForm } from './styles';
+import H2 from '../H2';
 
 const Landing = () => {
   const messages = {
@@ -24,30 +29,64 @@ const Landing = () => {
         <title>FireWork</title>
         <meta name="description" content="FireWork" />
       </Helmet>
-      <Provider>
-        <Hero color="black" bg="white">
-          <Heading>FireWork</Heading>
-          <Subhead>Stop wasting time hiring developers</Subhead>
-          <MailChimpForm>
-            <Mailchimp
-              action="https://gmail.us20.list-manage.com/subscribe/post?u=080e7d76c1af33a59c13126db&amp;id=36a30fa311"
-              fields={[
-                {
-                  name: 'EMAIL',
-                  placeholder: 'Your Email',
-                  type: 'email',
-                  required: true,
-                },
-              ]}
-              messages={messages}
-            />
-          </MailChimpForm>
-          <CallToAction onClick={window.gtag_report_conversion} href="/" mt={3}>
-            Connect your issues
-          </CallToAction>
-          {/* <ScrollDownIndicator /> */}
-        </Hero>
-      </Provider>
+      <Hero
+        bg="white"
+        bgOpacity={0.5}
+        backgroundImage="https://unsplash.com/photos/76uCfq8xPYU"
+      >
+        <Flex flexWrap="wrap" alignItems="center">
+          <Box width={800} p={3}>
+            <Section
+              color="black"
+              bg="white"
+              heading="FireWork"
+              subhead="Get work done instead of hiring developers"
+            >
+              <MailChimpForm>
+                <Mailchimp
+                  action="https://gmail.us20.list-manage.com/subscribe/post?u=080e7d76c1af33a59c13126db&amp;id=36a30fa311"
+                  fields={[
+                    {
+                      name: 'EMAIL',
+                      placeholder: 'email',
+                      type: 'email',
+                      required: true,
+                    },
+                  ]}
+                  messages={messages}
+                />
+              </MailChimpForm>
+              <H2 textalign="center">How does it work?</H2>
+              <Flex flexWrap="wrap" justifyContent="center">
+                <Feature
+                  icon="🗒"
+                  description="Connect your tasks with our API through Github or Jira"
+                >
+                  Connect your tasks
+                </Feature>
+                <Feature icon="💰" description="You pay how much you want">
+                  Prepay for each task
+                </Feature>
+                <Feature
+                  icon="👌"
+                  description="Tests and review show when task is complete"
+                >
+                  Code is tested
+                </Feature>
+              </Flex>
+              <Flex mt={3} flexWrap="wrap" justifyContent="center">
+                <CallToAction
+                  onClick={window.gtag_report_conversion}
+                  href="/"
+                  mt={3}
+                >
+                  Connect your tasks
+                </CallToAction>
+              </Flex>
+            </Section>
+          </Box>
+        </Flex>
+      </Hero>
     </div>
   );
 };
