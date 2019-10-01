83a0a2a22c7218b8fbde843761c57fb0eb955fc9
Keena7 <manjrekarkena7@gmail.com>
2019-10-01 13:38:31 +0530

    Credit card signup front end

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,6 +8,7 @@ import Login from 'containers/Login';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Repo from 'containers/RepoList/Loadable';
+import CreditCardSignup from '../CreaditCardSignupPage';
 import CRoute from './privateRoutes';
 
 import GlobalStyle from '../../global-styles';
@@ -30,6 +31,12 @@ function App() {
         <CRoute exact path="/" component={WorkPage} cprivate />
         <CRoute exact path="/login" component={Login} />
         <CRoute exact path="/repositories" component={Repo} cprivate />
+        <CRoute
+          exact
+          path="/credit-card-signup"
+          component={CreditCardSignup}
+          cprivate
+        />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
---
+++ app/containers/CreaditCardSignupPage/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  text-align: center;
+  button {
+    margin-top: 30px;
+  }
+`;
