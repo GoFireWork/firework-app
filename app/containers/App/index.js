c166878f348f10b988d31549b29156289e0ff0fb
JohnAllen <johnjalleniii@gmail.com>
2019-10-02 13:19:41 +0200

    Remove currently unused routes and JS tags

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -4,11 +4,9 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import Landing from 'containers/Landing';
-import WorkPage from 'containers/WorkPage/Loadable';
 import Login from 'containers/Login';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
-import Repo from 'containers/RepoList/Loadable';
 import CRoute from './privateRoutes';
 
 import GlobalStyle from '../../global-styles';
@@ -29,9 +27,7 @@ function App() {
       <Header />
       <Switch>
         <CRoute exact path="/" component={Landing} />
-        <CRoute exact path="/work" component={WorkPage} cprivate />
         <CRoute exact path="/login" component={Login} />
-        <CRoute exact path="/repositories" component={Repo} cprivate />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
