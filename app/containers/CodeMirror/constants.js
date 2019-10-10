6ecff1fa069306b9d2daf9314c9d64716b99b28a
JohnAllen <john@gofirework.com>
2019-10-10 08:27:53 -0700

    Add meta-description image Delete tons of old stuff Add email to Stripe form Delete reducers

--- app/containers/CodeMirror/constants.js
+++
@@ -1,4 +0,0 @@
-/*
-export const CODE_MIRROR_STATE = 'boilerplate/CodeMirror/CODE_MIRROR_STATE';
-export const CODE_MIRROR_ERROR = 'boilerplate/CodeMirror/CODE_MIRROR_ERROR';
-*/
--- app/containers/WorkPage/constants.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_REPO_URL = 'boilerplate/WorkPage/CHANGE_REPO_URL';
-export const LOAD_REPO_REQUEST = 'boilerplate/WorkPage/LOAD_REPO_REQUEST';
-export const LOAD_REPO_ERROR = 'boilerplate/WorkPage/LOAD_REPO_ERROR';
-export const LOAD_REPO_SUCCESS = 'boilerplate/WorkPage/LOAD_REPO_SUCCESS';
