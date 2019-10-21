fde964366a4617438f81a4ba4ad58883a57c71f5
JohnAllen <john@gofirework.com>
2019-10-21 14:22:24 -0700

    Remove unused language stuff

--- app/app.js
+++ app/app.js
@@ -12,7 +12,6 @@ import 'sanitize.css/sanitize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
 import App from 'containers/App';
-import LanguageProvider from 'containers/LanguageProvider';
 import { StripeProvider } from 'react-stripe-elements';
 
 // Load the favicon and the .htaccess file
@@ -20,9 +19,6 @@ import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
 
 import configureStore from './configureStore';
-
-// Import i18n messages
-import { translationMessages } from './i18n';
 
 export const apiUrl =
   process.env.NODE_ENV === 'production'
@@ -44,49 +40,20 @@ openSansObserver.load().then(() => {
 
 const MOUNT_NODE = document.getElementById('app');
 
-const render = messages => {
+const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <LanguageProvider messages={messages}>
-        <ConnectedRouter history={history}>
-          <StripeProvider apiKey="pk_test_8V1hXWIWlo9l6Sd6aeH46POA006CKa8izt">
-            <App />
-          </StripeProvider>
-        </ConnectedRouter>
-      </LanguageProvider>
+      <ConnectedRouter history={history}>
+        <StripeProvider apiKey="pk_test_8V1hXWIWlo9l6Sd6aeH46POA006CKa8izt">
+          <App />
+        </StripeProvider>
+      </ConnectedRouter>
     </Provider>,
     MOUNT_NODE,
   );
 };
 
-if (module.hot) {
-  // Hot reloadable React components and translation json files
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept(['./i18n', 'containers/App'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    render(translationMessages);
-  });
-}
-
-// Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  new Promise(resolve => {
-    resolve(import('intl'));
-  })
-    .then(() =>
-      Promise.all([
-        import('intl/locale-data/jsonp/en.js'),
-        import('intl/locale-data/jsonp/de.js'),
-      ]),
-    ) // eslint-disable-line prettier/prettier
-    .then(() => render(translationMessages))
-    .catch(err => {
-      throw err;
-    });
-} else {
-  render(translationMessages);
-}
+render();
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
